9eb19b2c257a96037a625a95bf2e7294b3523578
Dmitry <kulakov_dima26@mail.ru>
2021-10-31 20:23:41 +0300

    Fixed minutes

--- Homework 1/Lesson02.py
+++ Homework 1/Lesson02.py
@@ -1,8 +1,8 @@
 seconds = int(input("Введите количество секунд: "))
 
 if 0 <= seconds <= 356400:
-    minutes = seconds % 60
     hours = seconds // 3600
+    minutes = seconds % 3600 // 60
     seconds %= 60
     print("{:>02}:{:>02}:{:>02}".format(hours, minutes, seconds))
 else:

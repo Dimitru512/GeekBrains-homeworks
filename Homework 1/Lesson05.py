8c8f6bfa908c458449c74cb1db0cce6eeed9cd93
Dmitry <kulakov_dima26@mail.ru>
2021-10-31 14:29:27 +0300

    Выполнено 5 задание

---
+++ Homework 1/Lesson05.py
@@ -0,0 +1,12 @@
+proceeds = int(input("Введите значение выручки: "))
+outgoings = int(input("Введие значение издержек: "))
+
+if proceeds > outgoings:
+    print("Фирма получила прибыль")
+    print("Рентабельность выручки составляет " + str(proceeds / outgoings))
+    employees = int(input("Введите количество сотрудников: "))
+    print("Прибыль в фирме в расчете на одного сотрудника составляет " + str((proceeds - outgoings) / employees))
+elif proceeds < outgoings:
+    print("Фирма работает в убыток")
+else:
+    print("Фирма ушла в ноль")

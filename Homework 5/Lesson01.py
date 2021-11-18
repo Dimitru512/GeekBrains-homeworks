ce7177bccd121b0279be824a49019210dfa952e3
Dmitry Kulakov <kulakov_dima26@mail.ru>
2021-11-18 16:56:23 +0300

    Added instruction

--- Homework 5/Lesson01.py
+++ Homework 5/Lesson01.py
@@ -3,6 +3,8 @@
 
 with open("new_file.txt", "w") as file:
     x = None
+    print("Для выхода оставьте поле ввода пустым и нажмите Enter")
     while x != "":
         x = input("Введите текст: ")
         file.write(x + "\n")
+

b1410c33c1d7e281cc8cacdfbbae70b2d20def0a
Dmitry Kulakov <kulakov_dima26@mail.ru>
2021-11-18 17:53:58 +0300

    Update

--- Homework 5/Lesson03.py
+++ Homework 5/Lesson03.py
@@ -5,3 +5,14 @@
 # Иванов 23543.12
 # Петров 13749.32
 
+with open("les3_file.txt", "r") as file:
+    salary_sum = 0
+    row_count = 0
+    print("Сотрудники с окладом менее 20 тысяч:")
+    for row in file:
+        splited_row = row.split()
+        salary_sum += float(splited_row[1])
+        if float(splited_row[1]) < 20000:
+            print(row)
+        row_count += 1
+    print(f"Средняя зарплата: {salary_sum / row_count}")

9fc48e2255947fcdb7eef47f94605bf7d921c777
Dmitry <kulakov_dima26@mail.ru>
2021-11-28 10:38:43 +0300

    Update 28.11.21

--- Homework 6/Lesson03.py
+++ Homework 6/Lesson03.py
@@ -6,14 +6,22 @@
 # передать данные, проверить значения атрибутов, вызвать методы экземпляров).
 
 class Worker:
-    def __init__(self, name: str, surname: str, position: str, income: float):
+    def __init__(self, name: str, surname: str, position: str, income):
         self.name = name
         self.surname = surname
         self.position = position
-        self._income_wage = income
-        self._income_bonus = income
+        self._income_wage = income["wage"]
+        self._income_bonus = income["bonus"]
 
 
 class Position(Worker):
     def get_full_name(self):
-        print(Worker.)
+        print(f"Работник {self.surname} {self.name} - {self.position}")
+
+    def get_total_income(self):
+        print(f"Доход с учетом премии - {self._income_wage + self._income_bonus}")
+
+
+worker_1 = Position("John", "Doe", "driver", {"wage": 10000, "bonus": 5000})
+worker_1.get_full_name()
+worker_1.get_total_income()
